c42eac4caca8a023e0cd17fcdcbd0efe47e0ba40
NoHaxito <santifeb2034@gmail.com>
2023-12-20 21:50:07 -0500

    Update tailwind.config.ts

--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -11,20 +11,20 @@ const config: Config = {
     extend: {
       keyframes: {
         "accordion-down": {
-          from: { height: 0 },
-          to: { height: "var(--radix-accordion-content-height)" },
+          from: { "height": 0 },
+          to: { "height": "var(--radix-accordion-content-height)" },
         },
         "accordion-up": {
-          from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: 0 },
+          from: { "height": "var(--radix-accordion-content-height)" },
+          to: { "height": 0 },
         },
         "collapsible-down": {
-          from: { height: 0 },
-          to: { height: "var(--radix-collapsible-content-height)" },
+          from: { "height": 0 },
+          to: { "height": "var(--radix-collapsible-content-height)" },
         },
         "collapsible-up": {
-          from: { height: "var(--radix-collapsible-content-height)" },
-          to: { height: 0 },
+          from: { "height": "var(--radix-collapsible-content-height)" },
+          to: { "height": 0 },
         },
       },
       animation: {
